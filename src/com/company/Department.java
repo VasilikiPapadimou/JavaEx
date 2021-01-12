458cf9447660b17223fdaef53bb403de511232f3
Vasiliki Papadimou <33549847+VikyPapadimou@users.noreply.github.com>
2021-01-12 22:06:33 +0200

    Coments Department

--- src/com/company/Department.java
+++ src/com/company/Department.java
@@ -2,7 +2,19 @@ package com.company;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-
+/**
+ * Every Department HAS Employees and Master Employees, and HAS Jobs
+ * Every Employee that is Master is assigned ONLY in 1 Department
+ * Every Department can have more than 1 Jobs
+ *
+ * Constructor Department --> includes ALL types of Employees (Master) and Job
+ * Arraylist<Employee> getEmployees --> returns the employees from the Arraylist
+ * PaymentExpences() --> counts the amount of money payed FOR EACH Employee employee that is in the ArrayList
+ *                       and adds the exact amount accounted for the Masters
+ * MaxPureIncome --> counts the job that gives the Max incoming money to the company
+ * setMaster --> Sets a master from Master class as the Department master
+ * toString --> to visualise the outcome of this class 
+ **/
 public class Department implements Serializable {
 
     private Master master;
@@ -46,9 +58,23 @@ public class Department implements Serializable {
        return job.get(found);
     }
 
+    public void setMaster(Master master){this.master=master;}
+
+    public void setEmployees(Employee e){
+        employees.add(e);
+    } // add-> used for lists
+
+    public void SetJob(Job j)
+    {
+        this.job.add(j);
+    }
+
+
     public Master getMaster() {
         return master;
     }
+
+    public ArrayList<Employee> getEmployees() { return employees; }
 
     public ArrayList<Job> getJob() {
         return job;
@@ -62,22 +88,11 @@ public class Department implements Serializable {
         return description;
     }
 
+    //To move an Employee to another Department
+    public void ChangeDepartment(Department destinationdepart, Employee employee)  { }
+
     @Override
     public String toString() {
         return depName;
     }
-    public void SetJob(Job j)
-    {
-        this.job.add(j);
-    }
-
-    public void ChangeDepartment(Department destinationdepart, Employee employee)  { }
-
-    public void setMaster(Master master){this.master=master;}
-
-    public void setEmployees(Employee e){
-        employees.add(e);
-    }
-
-    public ArrayList<Employee> getEmployees() { return employees; }
 }
