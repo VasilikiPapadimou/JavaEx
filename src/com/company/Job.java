ebeaf7d395847fcc1f73b5db2340aa10274653f6
Vasiliki Papadimou <33549847+VikyPapadimou@users.noreply.github.com>
2021-01-15 11:48:54 +0200

    deleting unUsed Get from Job class

--- src/com/company/Job.java
+++ src/com/company/Job.java
@@ -81,15 +81,6 @@ public class Job implements Serializable {
     public String getDescription() {
         return description;
     }
-    public double getIncome() {
-        return income;
-    }
-    public double getOutcome() {
-        return outcome;
-    }
-    public double getFuncOut() {
-        return funcOut;
-    }
 
     public HashMap<Employee, Date[]> getJobEmp() { return jobEmp; }
 
